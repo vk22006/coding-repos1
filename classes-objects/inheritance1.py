6f635a954e842773dbe5d21caae26191999bee6a
vk22006 <kishorev22006@gmail.com>
2024-01-28 19:16:06 +0530

    Created file inheritance1.py

---
+++ classes-objects/inheritance1.py
@@ -0,0 +1,11 @@
+# Write a program to implement Single inheritance
+
+class dad:
+    def money():
+        print("Dad's money")
+class son(dad):
+    def laptop():
+        print("Son's laptop")
+
+Son = son()
+Son.money()
