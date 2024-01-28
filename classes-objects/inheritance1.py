bfdde1dec6952d1032431754a707a9bb308f0065
vk22006 <kishorev22006@gmail.com>
2024-01-28 19:21:57 +0530

    Added self argument in class methods

--- classes-objects/inheritance1.py
+++ classes-objects/inheritance1.py
@@ -1,10 +1,10 @@
 # Write a program to implement Single inheritance
 
 class dad:
-    def money():
+    def money(self):
         print("Dad's money")
 class son(dad):
-    def laptop():
+    def laptop(self):
         print("Son's laptop")
 
 Son = son()
